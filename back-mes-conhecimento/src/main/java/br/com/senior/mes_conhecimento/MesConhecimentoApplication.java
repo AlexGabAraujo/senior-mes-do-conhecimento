3640a12729339cb9408cae90209be59f1506766d
Alex Araujo <alexgabriel.araujo@hotmail.com>
2026-03-31 11:32:59 -0300

    adicionando projeto base

---
+++ back-mes-conhecimento/src/main/java/br/com/senior/mes_conhecimento/MesConhecimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senior.mes_conhecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesConhecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesConhecimentoApplication.class, args);
+	}
+
+}
---
+++ back-mes-conhecimento/src/test/java/br/com/senior/mes_conhecimento/MesConhecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.mes_conhecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesConhecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
